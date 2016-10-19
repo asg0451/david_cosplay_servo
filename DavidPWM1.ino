0c03a6cb62dabc1b8ff07520142fd0856451fbf2
Miles Frankel <miles.frankel@gmail.com>
2016-10-18 23:20:39 -0400

    stuff

--- DavidPWM1.ino
+++ DavidPWM1.ino
@@ -1,23 +1,46 @@
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+enum ServoState {
+  opened,
+  closed
+};
 
-int pos = 0;    // variable to store the servo position
+static int CLOSED_POS = 180;
+static int OPENED_POS = 0;
+
+Servo left;
+Servo right;
+
+ServoState leftState;
+ServoState rightState;
+
+ServoState getState(Servo s) {
+  int val = s.read();
+  if(val <= 180 && val >= 170) { return closed; };
+  if(val <= 10 && val >= 0) { return opened; }
+}
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  left.attach(9);
+  right.attach(10);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  leftState = getState(left);
+  rightState = getState(right);
+
+  // left transition table
+//  switch(leftState) {
+//    case opened: left.write(CLOSED_POS); break;
+//    case closed: left.write(OPENED_POS); break;
+//  }
+
+  // right transition table
+  switch(rightState) {
+    case opened: right.write(CLOSED_POS); break;
+    case closed: right.write(OPENED_POS); break;
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  
+  delay(3000);
 }

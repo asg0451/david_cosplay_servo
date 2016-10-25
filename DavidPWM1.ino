34622f8b89acc34e7750b19492d06af66f6def62
Miles Frankel <miles.frankel@gmail.com>
2016-10-25 19:14:49 -0400

    half

--- DavidPWM1.ino
+++ DavidPWM1.ino
@@ -3,7 +3,13 @@
 
 enum ServoState {
   opened,
-  closed
+  closed,
+  somewhere
+};
+
+enum State {
+  travelling,
+  chilling
 };
 
 static int CLOSED_POS = 180;
@@ -12,35 +18,60 @@ static int OPENED_POS = 0;
 Servo left;
 Servo right;
 
-ServoState leftState;
-ServoState rightState;
+static int buttonRight = 12;
 
-ServoState getState(Servo s) {
+State rstate;
+State lstate;
+
+ServoState rightServoPrevState;
+
+ServoState getServoState(Servo s) {
   int val = s.read();
   if(val <= 180 && val >= 170) { return closed; };
   if(val <= 10 && val >= 0) { return opened; }
+  return somewhere;
 }
 
 void setup() {
   left.attach(9);
   right.attach(10);
+  pinMode(buttonRight, INPUT);
+  pinMode(13, OUTPUT);
+  rstate = chilling;
+  rightServoPrevState = opened;
+  lstate = chilling;
+  right.write(CLOSED_POS);
 }
 
-void loop() {
-  leftState = getState(left);
-  rightState = getState(right);
-
-  // left transition table
-//  switch(leftState) {
-//    case opened: left.write(CLOSED_POS); break;
-//    case closed: left.write(OPENED_POS); break;
-//  }
-
-  // right transition table
-  switch(rightState) {
-    case opened: right.write(CLOSED_POS); break;
-    case closed: right.write(OPENED_POS); break;
-  }
+void rightLoop() {
+  if(rstate == travelling) { digitalWrite(13, HIGH); }
+  else { digitalWrite(13, LOW); }
   
-  delay(3000);
+  if(rstate == travelling) {
+    if(getServoState(right) == closed && rightServoPrevState == opened) { 
+      right.write(OPENED_POS);
+      rightServoPrevState = closed; 
+      rstate = chilling;
+    } 
+    else if(getServoState(right) == opened && rightServoPrevState == closed) { 
+      right.write(CLOSED_POS); 
+      rightServoPrevState = opened;
+      rstate = chilling;
+    }
+//    else {
+//      rstate = chilling; 
+//    }
+  }
+  else {
+    // pushed for first time
+    if(digitalRead(buttonRight) == HIGH) {
+      rstate = travelling;
+    }
+  }
+}
+
+
+void loop() {
+  rightLoop();  
+  delay(200);
 }
